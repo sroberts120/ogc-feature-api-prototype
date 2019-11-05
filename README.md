e8da84b88f9bceabc6c45b2c8056ac77646b5bc6
sroberts <sam.roberts@os.uk>
2019-11-05 11:09:42 -0500

    WFS3-001 added delpoyable jar and web config

--- README.md
+++
@@ -1,2 +0,0 @@
-# ogc-feature-api-prototype
-This repository contains a prototype implementation of the OGC Features API over some sample OS Topographic Data
--- src/main/java/co/uk/ordnancesurvey/exceptions/InvalidAcceptsTypeException.java
+++
@@ -1,9 +0,0 @@
-package co.uk.ordnancesurvey.exceptions;
-
-public class InvalidAcceptsTypeException extends RuntimeException {
-
-    public InvalidAcceptsTypeException(String acceptsType) {
-        super("Invalid accepts type : " + acceptsType + " : Only Application/json supported");
-    }
-
-}
